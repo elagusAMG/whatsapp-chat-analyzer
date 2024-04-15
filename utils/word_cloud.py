ff3e6ec6efec31e959a8dbf9b0d2c53c34e51d70
Agustin <agustinmarcelomoya@gmail.com>
2024-04-15 23:06:54 +0100

    Filter words less than 4 chars

--- utils/word_cloud.py
+++ utils/word_cloud.py
@@ -2,12 +2,19 @@ import matplotlib.pyplot as plt
 
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
+
+def filter_words(message):
+    words = message.split()
+    filtered_words = [word for word in words if len(word) >= 4]
+    return " ".join(filtered_words)
+
+
 def world_cloud(dataframe):
     # Remove messages containing "Media omitted"
     x = dataframe[~dataframe["message"].str.contains("Media omitted")]
 
     # Concatenate all cleaned messages into a single string
-    all_messages = x["message"].dropna().str.cat(sep=" ")
+    all_messages = x["message"].dropna().apply(filter_words).str.cat(sep=" ")
 
     # Generate word cloud
     wordcloud = WordCloud(
@@ -16,7 +23,7 @@ def world_cloud(dataframe):
         background_color="white",
         stopwords=STOPWORDS,
         min_font_size=10,
-    ).generate(all_messages)
+    ).generate("".join(all_messages))
 
     # Plot word cloud
     plt.figure(figsize=(8, 8))
